1505c2f03fc2324729e73ed9a0359fcc10b45a8e
elochness <pierre.francois@outlook.com>
2016-12-02 22:29:01 +0100

    Add Tax in Object Product

---
+++ src/AppBundle/Entity/TaxRate.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * TaxRate
+ *
+ * @ORM\Table(name="tax_rate")
+ * @ORM\Entity
+ */
+class TaxRate
+{
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="rate", type="float", precision=10, scale=0, nullable=false)
+     */
+    private $rate;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+
+}
+
