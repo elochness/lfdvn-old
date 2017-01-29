adfe4cec0515118bcb46ed191b8deb7dc2239f4c
elochness <pierre.francois@outlook.com>
2017-01-29 19:19:16 +0100

    add Article category

---
+++ src/AppBundle/Entity/ArticleCategory.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace AppBundle\Entity;
+
+/**
+ * Article
+ */
+class ArticleCategory
+{
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var integer
+     */
+    private $id;
+
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return ArticleCategory
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get String information of article category
+     *
+     * @return string Name of article category
+     */
+    public function __toString()
+    {
+      return $this->getName();
+    }
+}
