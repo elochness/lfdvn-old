dba36c1488fe6fa757133e7894bd7028e9d668ae
elochness <pierre.francois88@gmail.com>
2017-11-22 16:50:26 +0100

    Add UpdatedAt property in Category's Class

--- src/AppBundle/Entity/Category.php
+++ src/AppBundle/Entity/Category.php
@@ -21,6 +21,11 @@ class Category
      * @var boolean
      */
     private $enabled;
+    
+    /**
+     * @var \DateTime
+     */
+    private $updatedAt;
 
     /**
      * @var integer
@@ -33,7 +38,7 @@ class Category
     private $image;
     
     /**
-     * @Vich\UploadableField(mapping="product_images", fileNameProperty="image")
+     * @Vich\UploadableField(mapping="category_images", fileNameProperty="image")
      * @var File
      */
     private $imageFile;
@@ -55,6 +60,7 @@ class Category
     public function __construct()
     {
         $this->enabled = true;
+        $this->updatedAt = new \DateTime();
     }
     
     /**
@@ -136,6 +142,30 @@ class Category
     {
         return $this->enabled;
     }
+    
+    /**
+     * Set updatedAt
+     *
+     * @param \DateTime $updatedAt
+     *
+     * @return Product
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+    
+        return $this;
+    }
+    
+    /**
+     * Get updatedAt
+     *
+     * @return \DateTime
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
 
     /**
      * Get id
@@ -184,8 +214,16 @@ class Category
     
     public function setImageFile($image = null)
     {
-    	// $this->imageFile = $this->getValidFilename($image);
-    	$this->imageFile = $image;
+        // $this->imageFile = $this->getValidFilename($image);
+        $this->imageFile = $image;
+
+        // VERY IMPORTANT:
+        // It is required that at least one field changes if you are using Doctrine,
+        // otherwise the event listeners won't be called and the file is lost
+        if ($image) {
+            // if 'updatedAt' is not defined in your entity, use another property
+            $this->updatedAt = new \DateTime('now');
+        }
     }
     
     public function getImageFile()
